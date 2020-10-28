cb5e9128e550c94ec55036b59c6398a9b6aeab95
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2020-10-28 16:32:03 -0300

    Implement socket on & emit events

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+const cors = require('cors');
+
+app.use(cors());
+
+http.listen(5000, ()=>{
+    console.log("Running on port *5000")
+})
+
+io.on('connection', socket=>{
+    console.log(`a user connected ${socket.id}`)
+
+    socket.on("order-product", (product)=>{
+        Object.assign(product, {userId: socket.id})
+        socket.broadcast.emit("order-product", product)
+    })
+})
