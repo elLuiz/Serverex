abf1e9dd3e6c056aa87305b9507b7df515295dfc
Luiz Henrique <lu1zh3nr1qu34@gmail.com>
2020-11-03 11:02:27 -0300

    Memoized the Nav

--- client/src/Components/Nav/Nav.js
+++ client/src/Components/Nav/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../../assets/styles/Components/Nav/nav.css';
 
-export default function Nav() {
+const Nav = ()=>{
     return (
         <nav className="serverex-navbar">
             <h1 className="logo">SERVER</h1>
         </nav>
     )
 }
+
+export default React.memo(Nav);
